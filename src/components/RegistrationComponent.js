9494b1e83705539982fd76c2839167585bf89f7e
Tomas Mockaitis <m21tomas@gmail.com>
2021-12-23 11:45:34 +0200

    idejau vakcinas i lista

--- src/components/RegistrationComponent.js
+++ src/components/RegistrationComponent.js
@@ -12,10 +12,21 @@ class RegistrationComponent extends React.Component {
             pavarde: '',
             gimimoData: '',
             adresas: '',
-            telefonas: ''
+            telefonas: '',
+            centras: '',
+            vakcina: ''
         };
     }
 
+    centrasChange = e => {
+        const valueSelectedByUser = parseInt(e.target.value)
+        this.setState({ centras: valueSelectedByUser });
+    }
+
+    vakcinaChange = e => {
+        const valueSelectedByUser = parseInt(e.target.value)
+        this.setState({ vakcina: valueSelectedByUser });
+    }
 
     handleChange = (event) => {
         const target = event.target;
@@ -33,7 +44,9 @@ class RegistrationComponent extends React.Component {
             pavarde: this.state.pavarde,
             gimimoData: this.state.gimimoData,
             adresas: this.state.adresas,
-            telefonas: this.state.telefonas
+            telefonas: this.state.telefonas,
+            centras: this.state.centras,
+            vakcina: this.state.vakcina
         }
         registracijos.push(item)
     }
@@ -42,12 +55,12 @@ class RegistrationComponent extends React.Component {
         return (
             <><div className="container-fluid">
 
-                <div style={{display: "flex", textAlign: "center"}} className="row">
+                <div style={{ display: "flex", textAlign: "center" }} className="row">
                     <div className="col-12">
                         <img style={{
                             display: "flex", alignItems: "center",
                             textAlign: "center",
-                            height: 200   
+                            height: 200
                         }} src='https://d.newsweek.com/en/full/1679597/pfizer-coronavirus-vaccine.jpg' alt='pfizer-coronavirus-vaccine.jpg' />
                     </div>
 
@@ -87,6 +100,27 @@ class RegistrationComponent extends React.Component {
                             value={this.state.telefonas} onChange={this.handleChange} />
                     </div>
 
+                    <div className="mb-3">
+                        <label className="form-label">Vakcinacijos centras</label>
+                        <select class="form-select" id="vakcGroup01" onChange={this.centrasChange}>
+                            <option selected>pasirinkti vakcinavimo centrą...</option>
+                            <option value="1">LITEXPO, Laisvės pr. 5</option>
+                            <option value="2">Šeškinės poliklinika, Šeškinės g. 24</option>
+                            <option value="3">Benu vaistinė, Kedrų g.4</option>
+                        </select>
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label">Vakcina</label>
+                        <select class="form-select" id="vakcGroup02" onChange={this.vakcinaChange}>
+                            <option selected>pasirinkti vakciną...</option>
+                            <option value="1">Moderna (Spikevax)</option>
+                            <option value="2">Pfizer–BioNTech (Comirnaty)</option>
+                            <option value="3">Oxford–AstraZeneca (Vaxzevria)</option>
+                            <option value="4">Johnson-'n'-Johnson (Janssen)</option>
+                        </select>
+                    </div>
+
                     <button type="submit" className="btn btn-primary">Įvesti</button>
                 </form>
 
@@ -97,7 +131,7 @@ class RegistrationComponent extends React.Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-12">
-                            <p style={{ "paddingTop": "20px", "textAlign": "center", "fontWeight": "bold", "fontSize": "20px" }}>
+                            <p style={{ "paddingTop": "20px", "textAlign": "left", "fontWeight": "bold", "fontSize": "20px" }}>
                                 Jūsų įvesti duomenys:</p>
                         </div>
                     </div>
@@ -110,6 +144,8 @@ class RegistrationComponent extends React.Component {
                                         <p>{fig.gimimoData}</p>
                                         <p>{fig.adresas}</p>
                                         <p>{fig.telefonas}</p>
+                                        <p>{fig.centras}</p>
+                                        <p>{fig.vakcina}</p>
                                     </div>
                                 </div>
                             ))
